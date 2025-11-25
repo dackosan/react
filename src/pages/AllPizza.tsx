8a4108f05cbb6d2ea6e9360e3b4776cdd1920646
Gyorgy David <gyorgy.david2006@gmail.com>
2025-11-25 12:40:14 +0100

    pizzak

---
+++ src/pages/AllPizza.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import apiClient, { baseURL } from "../api/apiClient";
+import type { Pizza } from "../types/Pizza";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+
+const AllPizza = () => {
+  const navigate = useNavigate();
+
+  const [pizzak, setPizzak] = useState<Array<Pizza>>([]);
+  const [kosar, setKosar] = useState<Array<number>>(JSON.parse(localStorage.getItem("kosar") ?? "[]"));
+
+  useEffect(() => {
+    apiClient
+      .get("/pizzak")
+      .then((response) => setPizzak(response.data))
+      .catch(() => toast.error("A pizzák betöltése sikertelen volt"));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("kosar", JSON.stringify(kosar));
+  }, [kosar]);
+
+  const generateCard = (p: Pizza) => {
+    return (
+      <Col>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={`${baseURL}/kepek/${p.imageUrl}`} />
+          <Card.Body>
+            <Card.Title>{p.nev}</Card.Title>
+            <Card.Text>{p.leiras}</Card.Text>
+            <Button
+              onClick={() => navigate(`/pizza/${p.id}`)}
+              variant="success"
+            >
+              Megtekintés
+            </Button>
+            <Button
+              onClick={() => {
+                setKosar([...kosar, Number(p.id)]);
+                localStorage.setItem("kosar", p.nev);
+                toast.success("Sikeresen berakta a korsárba!");
+              }}
+              variant="info"
+            >
+              Kosárba
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  };
+
+  return (
+    <Container>
+      <Row xs={"auto"} md={"auto"} className="g-4">
+        {pizzak.map((i) => generateCard(i))}
+      </Row>
+    </Container>
+  );
+};
+
+export default AllPizza;
