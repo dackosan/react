0796cc7225b137ad0ff0547fd4a219fa9811a704
Gyorgy David <gyorgy.david2006@gmail.com>
2025-12-10 10:24:48 +0100

    kart

--- src/pages/Kosar.tsx
+++ src/pages/Kosar.tsx
@@ -1,59 +1,72 @@
-import { useEffect, useState } from "react";
-import type { Pizza } from "../types/Pizza";
-import apiClient from "../api/apiClient";
+import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
+import apiClient from "../api/apiClient";
+import type { Pizza } from "../types/Pizza";
 import { Button, Table } from "react-bootstrap";
-import { FaTrashAlt } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
 
-const Kosar = () => {
+const Cart = () => {
   const [pizzak, setPizzak] = useState<Array<Pizza>>([]);
+  useEffect(() => {
+    apiClient
+      .get("/pizzak")
+      .then((response) => setPizzak(response.data))
+      .catch(() => toast.error("A pizzák betöltése sikertelen volt"));
+  }, []);
+
   const [kosar, setKosar] = useState<Array<number>>(
     JSON.parse(localStorage.getItem("kosar") ?? "[]")
   );
 
   useEffect(() => {
-    apiClient
-      .get(`/pizzak`)
-      .then((response) => setPizzak(response.data))
-      .catch(() => toast.error("A pizzák betöltése sikertelen volt"));
-  }, []);
-
-  useEffect(() => {
     localStorage.setItem("kosar", JSON.stringify(kosar));
   }, [kosar]);
 
-  const removeItem = (index: number) => {
-    setKosar(kosar.filter((v, i) => i !== index));
+  const removeItem = (searchedIndex: number) => {
+    setKosar(kosar.filter((v, i) => i !== searchedIndex));
   };
 
   return (
-    <Table>
-      <thead>
-        <th>Név</th>
-        <th>Ár</th>
-      </thead>
-      <tbody>
-        {kosar.map((id, index) => {
-          const pizza = pizzak.find((p) => p.id === id);
-          return (
-            <tr>
-              <td>{pizza?.nev}</td>
-              <td>{pizza?.ar}</td>
-              <td>
-                <Button variant="danger" onClick={() => removeItem(index)}>
-                  <FaTrashAlt />
-                </Button>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-      <tfoot>
-        <th></th>
-        <th>{}</th>
-      </tfoot>
-    </Table>
+    <>
+      <h1>Kosár tartalma</h1>
+      {kosar.length > 0 ? (
+        <>
+          <Table striped bordered hover>
+            <thead>
+              <th>Név</th>
+              <th>Ár</th>
+              <th>Törlés</th>
+            </thead>
+            <tbody>
+              {kosar.map((id, index) => {
+                const pizza = pizzak.find((p) => p.id == id);
+
+                return (
+                  <tr>
+                    <td>{pizza?.nev}</td>
+                    <td>{pizza?.ar} Ft</td>
+                    <td>
+                      <Button
+                        onClick={() => removeItem(index)}
+                        variant="danger"
+                      >
+                        <FaTrash />
+                      </Button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+          <Button onClick={() => setKosar([])} variant="warning">
+            Kiürítés
+          </Button>
+        </>
+      ) : (
+        <h2>A kosár tartalma üres</h2>
+      )}
+    </>
   );
 };
 
-export default Kosar;
+export default Cart;
